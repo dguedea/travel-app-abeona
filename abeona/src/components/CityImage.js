7f97d7c433dab3b2acffb647bc579e426b4edb01
dguedea <guedead@oregonstate.edu>
2021-10-19 20:41:29 +0200

    added dynamic image fetch

--- abeona/src/components/CityImage.js
+++ abeona/src/components/CityImage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./CityImage.css";
 import airplane from "../assets/airplane.jpg";
@@ -9,7 +9,10 @@ const CityImage = (props) => {
     <React.Fragment>
       {typeof props.destination.destCity != "undefined" && (
         <div className="city-image-item">
-          <img src={airplane} alt="test image" />
+          <img
+            src={`https://lamjenni-image.herokuapp.com/${props.destination.destCity},500,300`}
+            alt="test image"
+          />
           <div className="city-image-description">
             Grab photo of {props.destination.destCity} from microservice
           </div>
