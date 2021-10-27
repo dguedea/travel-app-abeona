b26e9e617c2c78a8d5dde3d33f5c481133ebf1e5
dguedea <guedead@oregonstate.edu>
2021-10-27 10:39:15 +0200

    updated timezone image

--- abeona/src/components/TimeZoneOutput.js
+++ abeona/src/components/TimeZoneOutput.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import "./TimeZoneOutput.css";
-import clock from "../assets/clock.jpeg";
+import timezone from "../assets/timezone.svg";
 
 const TimeZoneOutput = (props) => {
   const apikey = "OF86B8KNN9JF";
@@ -29,16 +29,14 @@ const TimeZoneOutput = (props) => {
         {time.status != "FAILED" ? (
           <div>
             <h2>Time now in {props.cityInput.destCity}</h2>
-            <div className="clock">
-              <img className="clock-image" src={clock} />
-            </div>
             <div className="city-time-description">
               <p>
                 {time.zoneName}, {time.abbreviation}
               </p>
-              <p>
-                {time.formatted}
-              </p>
+              <p>{time.formatted}</p>
+            </div>
+            <div className="clock">
+              <img className="clock-image" src={timezone} />
             </div>
           </div>
         ) : (
